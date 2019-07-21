9ff979a675ac402c414ec3869019b2cfa2306928
Gabriel Granata <ggran067@uottawa.ca>
2019-07-20 22:04:25 -0400

    adds basic onClick listener for page action object. removes panel in favour of 1 click UX

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const extensionId = 'aompemfgpnekdfaeohbeffbimnnbeiod';
+
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ on: true }, function () {
+    chrome.storage.sync.set({ enabled: true }, function () {
         console.log('hello world!');
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
@@ -14,3 +16,49 @@ chrome.runtime.onInstalled.addListener(function () {
         }]);
     });
 });
+
+chrome.pageAction.onClicked.addListener(click => {
+
+    chrome.storage.sync.get(['enabled'], result => {
+
+        let enabled = result.enabled;
+
+        if (enabled) {
+            console.log('The app is on, turning it off');
+            chrome.storage.sync.set({ enabled: false });
+            chrome.windows.getAll(windows => {
+                windows.forEach(window => {
+                    chrome.tabs.getAllInWindow(window.id, tabs => {
+                        tabs.forEach(tab => {
+                            chrome.pageAction.hide(tab.id, function() {
+                                console.log('hidden page action in tab ' + tab.id);
+                            })
+                        })
+                    });
+                });
+            });
+            // chrome.pageAction.hide(extensionId, function(res){
+            //     console.log(res);
+            // })
+        } else {
+            console.log('The app is off, turning it on');
+            chrome.storage.sync.set({ enabled: true });
+            chrome.windows.getAll(windows => {
+                windows.forEach(window => {
+                    chrome.tabs.getAllInWindow(window.id, tabs => {
+                        tabs.forEach(tab => {
+                            chrome.pageAction.show(tab.id, function() {
+                                console.log('showing page action in tab ' + tab.id);
+                            })
+                        })
+                    })
+                })
+            })
+        };
+    });
+
+
+    
+    
+});
+
