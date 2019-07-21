437a52df67b19a123292787b147ebcbe04e1b2b4
Gabriel Granata <ggran067@uottawa.ca>
2019-07-21 17:25:12 -0400

    adds IEX stock API calls, testing

--- background.js
+++ background.js
@@ -1,13 +1,34 @@
 'use strict';
 
-const extensionId = 'aompemfgpnekdfaeohbeffbimnnbeiod';
-
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ enabled: true }, function () {
         console.log('hello world!');
     });
-    
-    chrome.browserAction.setPopup({ popup: 'popup.html' }, function() {
+
+    chrome.browserAction.setPopup({ popup: 'popup.html' }, function () {
         console.log('popup has been set up');
     })
 });
+
+retriveData();
+
+function retriveData() {
+
+    chrome.storage.sync.get(['enabled'], function (items) {
+        if (items.enabled) {
+            makeStockDataRequest();
+        }
+    });
+}
+
+function makeStockDataRequest() {
+    const stockRequest = new XMLHttpRequest();
+    stockRequest.open('GET', 'https://sandbox.iexapis.com/stable/stock/GOOGL/quote?token=Tpk_fb93bef773284e5c84796dafe7f621df');
+    stockRequest.onreadystatechange = () => {
+        if (stockRequest.readyState === 4) {
+            let data = JSON.parse(stockRequest.responseText);
+            console.log(data);
+        }
+    }
+    stockRequest.send()
+}
