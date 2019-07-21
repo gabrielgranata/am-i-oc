cac3eab8e2832280b512af1bc8624fca9433fb45
Gabriel Granata <ggran067@uottawa.ca>
2019-07-21 14:23:13 -0400

    changes entire project. now developing a stock price alert app

--- background.js
+++ background.js
@@ -6,59 +6,8 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ enabled: true }, function () {
         console.log('hello world!');
     });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'www.reddit.com', schemes: ['https'] },
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});
-
-chrome.pageAction.onClicked.addListener(click => {
-
-    chrome.storage.sync.get(['enabled'], result => {
-
-        let enabled = result.enabled;
-
-        if (enabled) {
-            console.log('The app is on, turning it off');
-            chrome.storage.sync.set({ enabled: false });
-            chrome.windows.getAll(windows => {
-                windows.forEach(window => {
-                    chrome.tabs.getAllInWindow(window.id, tabs => {
-                        tabs.forEach(tab => {
-                            chrome.pageAction.hide(tab.id, function() {
-                                console.log('hidden page action in tab ' + tab.id);
-                            })
-                        })
-                    });
-                });
-            });
-            // chrome.pageAction.hide(extensionId, function(res){
-            //     console.log(res);
-            // })
-        } else {
-            console.log('The app is off, turning it on');
-            chrome.storage.sync.set({ enabled: true });
-            chrome.windows.getAll(windows => {
-                windows.forEach(window => {
-                    chrome.tabs.getAllInWindow(window.id, tabs => {
-                        tabs.forEach(tab => {
-                            chrome.pageAction.show(tab.id, function() {
-                                console.log('showing page action in tab ' + tab.id);
-                            })
-                        })
-                    })
-                })
-            })
-        };
-    });
-
-
     
-    
+    chrome.browserAction.setPopup({ popup: 'popup.html' }, function() {
+        console.log('popup has been set up');
+    })
 });
-
