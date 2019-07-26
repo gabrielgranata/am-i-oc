912a10f1532c11e7ab8ef0bdc47eacbff034f7b3
Gabriel Granata <ggran067@uottawa.ca>
2019-07-26 17:48:30 -0400

    turns request into a synchronous call

--- background.js
+++ background.js
@@ -4,8 +4,11 @@ setInterval(function () {
         let stocks = items.stocks;
         console.log(stocks);
         stocks.forEach(async function (stock) {
-            let latestPrice = await makeStockDataRequest(stock.ticker);
+            let stockRequest = await retriveStockData(stock.ticker);
+            let data = JSON.parse(stockRequest.responseText);
+            let latestPrice = data.latestPrice;
             console.log(latestPrice);
+
             if (stock.high) {
                 await checkHigh(latestPrice, stock.high, stock.ticker);
             } else if (stock.low) {
@@ -25,21 +28,10 @@ chrome.runtime.onInstalled.addListener(function () {
 
 });
 
-function getPrices() {
-    chrome.storage.sync.get(['tickers'], function (items) {
-        let tickers = items.tickers
-        tickers.forEach(function (element) {
-            element.price = makeStockDataRequest(element.ticker);
-            chrome.storage.sync.set({ tickers: tickers })
-        });
-    });
-}
+async function retriveStockData(stockName) {
 
-async function makeStockDataRequest(stockName) {
-    let latestPrice;
-    let done;
     const stockRequest = new XMLHttpRequest();
-    stockRequest.open('GET', 'https://sandbox.iexapis.com/stable/stock/' + stockName + '/quote?token=Tpk_fb93bef773284e5c84796dafe7f621df');
+    stockRequest.open('GET', 'https://sandbox.iexapis.com/stable/stock/' + stockName + '/quote?token=Tpk_fb93bef773284e5c84796dafe7f621df', false);
     stockRequest.onreadystatechange = () => {
         if (stockRequest.readyState === 4) {
             let data = JSON.parse(stockRequest.responseText);
@@ -47,14 +39,9 @@ async function makeStockDataRequest(stockName) {
             done = true;
         }
     }
-
-    await setTimeout(function() {
-        console.log('hi');
-    }, 5000);
-
     stockRequest.send();
-
-    return latestPrice;
+    return stockRequest;
+    
 
 }
 
